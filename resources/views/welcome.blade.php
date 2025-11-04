86fa542a427dccf0c8e33d92c88c67fe96af6609
Héritier Mulume <heritiermulume@gmail.com>
2025-11-04 07:22:58 +0100

    fix: Changer le titre de l'onglet navigateur en 'Compte Herime'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>HERIME SSO - Authentification centralisée</title>
+        <title>Compte Herime</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
